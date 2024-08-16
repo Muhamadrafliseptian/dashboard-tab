f2eaaccf9bad22fc2d1261ff72f3e28b855ba407
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2024-08-16 10:24:58 +0700

    push

---
+++ app/Http/Helper/ApiHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Helper;
+
+class ApiHelper
+{
+    protected static $baseUrl;
+
+    public static function init()
+    {
+        self::$baseUrl = "http://192.168.100.104:3001/api";
+    }
+
+    public static function baseUrl()
+    {
+        if (!self::$baseUrl) {
+            self::init();
+        }
+
+        return self::$baseUrl;
+    }
+
+    public static function apiUrl($path)
+    {
+        return self::baseUrl() . '/' . ltrim($path, '/');
+    }
+}
